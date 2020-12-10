ba2255ae3c1b4f5153b3975d59b47a65aaf2ad0a
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-10 15:07:55 +0300

    introducing routers

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require ('express')
 const app = express()
 
+const router = require('./router')
+console.log(router)
+
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
