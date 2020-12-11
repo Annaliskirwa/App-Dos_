88e3e34ca5ce921944ce111044cc6feb9875ed07
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-11 15:32:46 +0300

    username validation

--- models/User.js
+++ models/User.js
@@ -7,8 +7,10 @@ User.prototype.validate = function(){
     if (this.data.username == "") {this.errors.push("You must provide a username")}
     if (this.data.email == "") {this.errors.push("You must provide a valid email")}
     if (this.data.password == "") {this.errors.push("You must provide a valid password")}
-    if(this.data.password.length > 0 && this.data.password.length < 12){this.errors.push("Please enter a password that is more than 12 characters")}
-    if( ){}
+    if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Please enter a password that is more than 12 characters")}
+    if (this.data.password.length > 100){this.errors.push("Password cannot be more than 100 characters")}
+    if (this.data.username.length > 0 && this.data.password.length < 3) {this.errors.push("Please enter a username that is more than 3 characters")}
+    if (this.data.username.length > 30){this.errors.push("username cannot be more than 30 characters")}
 }
 
 User.prototype.register = function(){
