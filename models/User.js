578b1b942772d4fe218f296f4973f0808780dc9b
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-11 14:58:04 +0300

    validation changes

--- models/User.js
+++ models/User.js
@@ -5,8 +5,8 @@ this.errors = []
 
 User.prototype.validate = function(){
     if (this.data.username == "") {this.errors.push("You must provide a username")}
-    if (this.data.email == "") {this.errors.push("You must provide a valid password")}
-    if (this.data.password == "") {this.errors.push("You must provide a password")}
+    if (this.data.email == "") {this.errors.push("You must provide a valid email")}
+    if (this.data.password == "") {this.errors.push("You must provide a valid password")}
 }
 
 User.prototype.register = function(){
