c5520938a513a314d408929c7197e1a757b12da8
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-11 14:51:29 +0300

    adding validation to the model

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -11,7 +11,12 @@ exports.logout = function(){
 exports.register = function(req, res){
   let user = new User(req.body)
   user.register()
-   res.send("Thanks for trying to register") 
+  if(user.errors.length){
+res.send(user.errors)
+  }
+  else{
+res.send("Congrats, there are no errors")
+  }
 }
 
 exports.home = function(req, res){
--- models/User.js
+++ models/User.js
@@ -1,9 +1,19 @@
 let User = function(data){
 this.data = data
+this.errors = []
+}
+
+User.prototype.validate = function(){
+    if (this.data.username == "") {this.errors.push("You must provide a username")}
+    if (this.data.email == "") {this.errors.push("You must provide a valid password")}
+    if (this.data.password == "") {this.errors.push("You must provide a password")}
 }
 
 User.prototype.register = function(){
-    
+    // Step One: Validate user validation errors
+    this.validate()
+    //Step Two: Only if there are no validation errors
+    //then save user data  to database
 }
 
 module.exports = User
