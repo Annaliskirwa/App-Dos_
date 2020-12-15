8690fc0fc0c9b6e440f8b67023b07e8226a9b5fe
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-15 13:27:53 +0300

    starting express app when db is ready

--- app.js
+++ app.js
@@ -13,4 +13,4 @@ app.set('view engine', 'ejs')
 
 app.use('/', router)
 
-app.listen(3000)
+module.exports = app 
--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongodb = require ('mongodb')
 
-const connectionString = ''
+const connectionString = 'mongodb+srv://todoAppUser:kirwa9431@cluster0.sftiy.mongodb.net/ComplexApp?retryWrites=true&w=majority'
 
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
-
+module.exports = client.db()
+const app = require('./app')
+app.listen(3000)
 }) 
