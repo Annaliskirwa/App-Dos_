13a15332b8a567cbb4917c901b7b33b2b7cde48b
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-15 11:03:00 +0300

    email vaildation

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ this.errors = []
 
 User.prototype.validate = function(){
     if (this.data.username == "") {this.errors.push("You must provide a username")}
-    if (this.data.email == "") {this.errors.push("You must provide a valid email")}
+    if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email")}
     if (this.data.password == "") {this.errors.push("You must provide a valid password")}
     if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Please enter a password that is more than 12 characters")}
     if (this.data.password.length > 100){this.errors.push("Password cannot be more than 100 characters")}
