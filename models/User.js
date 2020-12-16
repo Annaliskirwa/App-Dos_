2a377a04f53b0415e1c9a95a826bbafb45761c11
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-16 12:57:23 +0300

    database to use a promise

--- models/User.js
+++ models/User.js
@@ -33,12 +33,14 @@ User.prototype.validate = function(){
 User.prototype.login = function (){
     return new Promise((resolve, reject) => {
         this.cleanUp()
-        usersCollection.findOne({username: this.data.username}, (err, attemptedUser) => {
+        usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
             if (attemptedUser && attemptedUser.password == this.data.password){
-     resolve("Congrats")
-            } else {
-     reject("Invalid username/ password")
-            }
+                resolve("Congrats")
+                       } else {
+                reject("Invalid username/ password")
+                       }
+        }).catch(function(){
+            reject("Please try again later")
         })
     })
 }
