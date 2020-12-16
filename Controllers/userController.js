eaaae39ae8c48b158a318591fe12f5462dec3e6f
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-16 12:41:55 +0300

    leveraging promises

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -2,8 +2,10 @@ const User = require('../models/User')
 
 exports.login = function(req, res){
 let user = new User(req.body)
-user.login(function(result){
-res.send(result)
+user.login().then(function(result){
+  res.send(result)
+}).catch(function(e){
+  res.send(e)
 })
 }
 
