5894f9483fe0893882f749e494cd1a93aa3281ae
Annaliskirwa <annaliskirwa@gmail.com>
2020-12-16 12:27:04 +0300

    promise to replace traditional callback

--- models/User.js
+++ models/User.js
@@ -30,15 +30,17 @@ User.prototype.validate = function(){
     if (this.data.username.length > 30){this.errors.push("username cannot be more than 30 characters")}
 }
 
-User.prototype.login = function (callback){
-    this.cleanUp()
-   usersCollection.findOne({username: this.data.username}, (err, attemptedUser) => {
-       if (attemptedUser && attemptedUser.password == this.data.password){
-callback("Congrats")
-       }else{
-callback("Invalid username/ password")
-       }
-   })
+User.prototype.login = function (){
+    return new Promise(function(resolve, reject){
+        this.cleanUp()
+        usersCollection.findOne({username: this.data.username}, (err, attemptedUser) => {
+            if (attemptedUser && attemptedUser.password == this.data.password){
+     resolve("Congrats")
+            }else{
+     reject("Invalid username/ password")
+            }
+        })
+    })
 }
 
 User.prototype.register = function(){
