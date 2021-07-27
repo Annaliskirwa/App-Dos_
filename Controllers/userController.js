43601b55c8e0070d0145902bb4d12b61d6a58184
Annaliskirwa <annaliskirwa@gmail.com>
2021-07-27 21:34:39 +0300

    using flash messages for registration errs

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -27,7 +27,12 @@ exports.register = function(req, res){
   let user = new User(req.body)
   user.register()
   if(user.errors.length){
-res.send(user.errors)
+user.errors.forEach(function(error){
+req.flash('regErrors', error)
+})
+req.session.save(function(){
+  res.redirect('/')
+})
   }
   else{
 res.send("Congrats, there are no errors")
@@ -38,6 +43,6 @@ exports.home = function(req, res){
   if (req.session.user) {
 res.render('home-dashboard', {username: req.session.user.username})
   } else {
-    res.render('home-guest', {errors: req.flash('errors')})
+    res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
   }
 }
