fb2c99bcf6e22ec3ec97b39dd4d9593d8b78e855
Annaliskirwa <annaliskirwa@gmail.com>
2021-07-27 23:57:06 +0300

    changing users profile pictures

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const usersCollection = require ('../db').db().collection("users")
 const validator = require("validator")
 const bcrypt = require ("bcryptjs")
+const md5 = require('md5')
 
 let User = function(data){
 this.data = data
@@ -50,6 +51,7 @@ User.prototype.login = function (){
         this.cleanUp()
         usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
             if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)){
+                this.getAvatar()
                 resolve("Congrats")
                        } else {
                 reject("Invalid username/ password")
@@ -72,12 +74,17 @@ User.prototype.register = function(){
             let salt = bcrypt.genSaltSync(10)
             this.data.password = bcrypt.hashSync(this.data.password, salt)
             await usersCollection.insertOne(this.data)
+            this.getAvatar()
             resolve()
         }
         else {
             reject(this.errors)
         }
     })
+}
+
+User.prototype.getAvatar = function(){
+    this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
 }
 
 module.exports = User
