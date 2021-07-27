2a8bbbe830c355a5f6293623845e1de76fd3ed9b
Annaliskirwa <annaliskirwa@gmail.com>
2021-07-27 16:01:42 +0300

    storing session data in the database

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
-module.exports = client.db()
+module.exports = client
 const app = require('./app')
 app.listen(process.env.PORT)
 }) 
