7fc22afc298716161160051e831764c4f8d61bd2
Annaliskirwa <annaliskirwa@gmail.com>
2021-07-29 00:19:15 +0300

    creating a header template for all pages

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -1,3 +1,3 @@
 exports.viewCreateScreen = function(req, res){
-    res.render('create-post')
+    res.render('create-post',{username: req.session.user.username, avatar: req.session.user.avatar})
 }
