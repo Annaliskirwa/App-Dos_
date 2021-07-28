d5869e50ac52b2d292a66f2b69d0ad5cc9687791
Annaliskirwa <annaliskirwa@gmail.com>
2021-07-28 23:45:21 +0300

    create post button linking o template

--- router.js
+++ router.js
@@ -1,10 +1,15 @@
 const express = require ('express')
 const router = express.Router()
 const userController = require('./Controllers/userController')
+const postController = require('./Controllers/postController')
 
+//user realted routes
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
+
+//post related routes
+router.get('/create-post', postController.viewCreateScreen)
 
 module.exports = router 
