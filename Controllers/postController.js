65f3181644bc55e7967af7ebe3286aaf65c2f8b4
Annaliskirwa <annaliskirwa@gmail.com>
2021-07-29 17:15:19 +0300

    users saving posts into database

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -1,3 +1,15 @@
+const Post = require('../models/Post')
+
 exports.viewCreateScreen = function(req, res){
     res.render('create-post')
+}
+
+exports.create = function(req, res){
+    let post = new Post(req.body)
+    post.create().then(function(){
+        res.send("New post has been created")
+
+    }).catch(function(errors){
+        res.send(errors)
+    })
 }
