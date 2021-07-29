beefb63e0091082176bdb5564a819299ad7a1532
Annaliskirwa <annaliskirwa@gmail.com>
2021-07-29 15:52:01 +0300

    user created posts

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -56,7 +56,7 @@ exports.register = function(req, res){
 
 exports.home = function(req, res){
   if (req.session.user) {
-res.render('home-dashboard', {username: req.session.user.username, avatar: req.session.user.avatar})
+res.render('home-dashboard')
   } else {
     res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
   }
--- app.js
+++ app.js
@@ -17,6 +17,12 @@ cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
 app.use(sessionOptions)
 app.use(flash())
 
+app.use(function(req, res, next){
+    res.locals.user = req.session.user
+    next()
+
+})
+
 const router = require('./router')
 
 app.use(urlencoded({extended: false}))
