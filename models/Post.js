b68daac2414bf7732289d84dd5e90bf09108db94
Annaliskirwa <annaliskirwa@gmail.com>
2021-07-30 20:46:16 +0300

    looking up a post

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
 const postsCollection = require('../db').db().collection("posts")
 const ObjectID = require('mongodb').ObjectID
+const User = require('./User')
 
 let Post = function(data){
     this.data = data
@@ -50,13 +51,27 @@ Post.findSingleById = function(id){
         }
         let posts = await postsCollection.aggregate([
             {$match: {_id: new ObjectID(id)}},
-            {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}}
+            {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
+            {$project: {
+                title: 1,
+                body: 1,
+                createdDate: 1,
+                author: {$arrayElemAt: ["$authorDocument", 0]}
+            }}
         ]).toArray()
+        //clean up author property in each post object
+        posts = posts.map(function(post){
+            post.author = {
+                username: post.author.username,
+                avatar:new User(post.author, true).avatar
+            }
+            return post
+        })
         if (posts.length){
             console.log(posts[0])
             resolve(posts[0])
         }else{
-            reject()
+            reject(errors)
         }
     })
 
