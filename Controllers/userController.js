c9fec799aa65b6bd89177f90e695d2d342ec3e30
Annaliskirwa <annaliskirwa@gmail.com>
2021-07-31 18:11:07 +0300

    fatching profile templates

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -63,7 +63,12 @@ res.render('home-dashboard')
 }
 
 exports.ifUserExists = function (req, res, next){
-  next()
+  User.findByUsername(req.params.username).then(function(userDocument){
+    req.profileUser = userDocument
+    next()
+  }).catch(function(){
+    res.render('404')
+  })
 }
 
 exports.profilePostsScreen = function (req, res){
