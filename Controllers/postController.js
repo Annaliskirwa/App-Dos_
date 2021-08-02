7c69914e4550897a98ae9878174535ac098bf582
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-02 23:18:08 +0300

    updating posts in a database

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -28,6 +28,31 @@ exports.viewEditScreen = async function(req, res){
     }
    catch{
        res.render('404')
+    }
+}
+exports.edit = function(res, req){
+    let post = new Post(req.body, req.visitorId, req.params.id)
+    post.update().then((status)=>{
+        //if its the real owner of the post
+        //if there are no validation errors 
+        if (status == "success"){
+            //post was updated in the database
+            req.flash("success", "Post was succesfully added to the database")
+        }else {
+            post.errors.forEach(function(error){
+                req.flash("errors", error)
+            })
+            req.session.save(function(){
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
 
-   }
+        }
+
+    }).catch(()=>{
+        // when the visitor is not the owner of the post to be edited
+        req.flash("errors", "You do not have permission to perform that action")
+        req.session.save(function(){
+            res.redirect("/")
+        })
+    })
 }
--- router.js
+++ router.js
@@ -17,5 +17,6 @@ router.get('/create-post', userController.mustBeLoggedIn, postController.viewCre
 router.post('/create-post', userController.mustBeLoggedIn, postController.create)
 router.get('/post/:id', postController.viewSingle)
 router.get('/post/:id/edit', postController.viewEditScreen)
+router.post('/post/:id/edit', postController.edit)
 
 module.exports = router 
