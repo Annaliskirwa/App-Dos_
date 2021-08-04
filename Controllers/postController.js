b9082ff469a2a2447ca3bc004513975844451b2f
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-04 20:13:46 +0300

    newly created post screen

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -6,10 +6,12 @@ exports.viewCreateScreen = function(req, res) {
 
 exports.create = function(req, res) {
   let post = new Post(req.body, req.session.user._id)
-  post.create().then(function() {
-    res.send("New post created.")
+  post.create().then(function(newId) {
+    req.flash("success", "New post has been succesfully created")
+    req.session.save(() => {res.redirect(`/post/${newId}`)})
   }).catch(function(errors) {
-    res.send(errors)
+    errors.forEach(error => req.flash("errors", error))
+    req.session.save(() => res.redirect("/create-post"))
   })
 }
 
