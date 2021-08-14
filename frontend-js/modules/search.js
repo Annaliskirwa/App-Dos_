0bfaf644a191abd02494238ae8146a08a8969116
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-15 02:09:41 +0300

    enabling user to type in search

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -5,6 +5,7 @@ export default class Search {
         this.headerSearchIcon = document.querySelector(".header-search-icon")
         this.overlay = document.querySelector(".search-overlay")
         this.closeIcon = document.querySelector(".close-live-search")
+        this.inputField = document.querySelector("#live-search-field")
         this.events()
      
     }
@@ -20,6 +21,7 @@ export default class Search {
     //3. Methods
     openOverlay(){
         this.overlay.classList.add("search-overlay--visible")
+        setTimeout(()=> this.inputField.focus(),50)
     }
     closeOverlay(){
         this.overlay.classList.remove("search-overlay--visible")
