cb55cc32b218643757f7777d831c9e384de8c394
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-15 00:47:52 +0300

    frontend javascript for the search feature

---
+++ public/main-bundled.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";new function n(){!function(n,e){if(!(n instanceof e))throw new TypeError("Cannot call a class as a function")}(this,n),alert("Search js is successfully being executed")}})();
