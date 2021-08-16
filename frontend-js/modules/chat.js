a5d1aeb7401f7b23f054a2c75dee5841c2771c4c
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-17 01:05:54 +0300

    added the invisible chat

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -1,11 +1,21 @@
 export default class Chat {
     constructor(){
-        alert("Chat feature is loading")
+       this.chatWrapper = document.querySelector("#chat-wrapper")
+       this.injectHTML()
         this.events()
     }
     //Events
     events(){}
 
     //Methods
+    injectHTML(){
+        this.chatWrapper.innerHTML = `
+        <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
+        <div id = "chat" class = "chat-log"></div>
+        <form id="chatForm" class="chat-form border-top">
+            <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
+        </form>
+        `
+    }
    
 }
