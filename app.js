82011e67066d7712ee5d53ce75eab624d0f6e8dc
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-18 11:48:36 +0300

    logging chat message to console

--- app.js
+++ app.js
@@ -50,8 +50,10 @@ const server = require ('http').createServer(app)
 
 const io = require ('socket.io')(server)
 
-io.on('connection', function (){
-  console.log("A new user has been connected")
+io.on('connection', function (socket){
+  socket.on('chatMessageFromBrowser',function(data){
+    console.log(data.message)
+  })
 })
 
 module.exports = server
