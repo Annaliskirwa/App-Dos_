8786c8249d76610e581b09717a883a9db82d32b9
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-18 22:05:22 +0300

    removing validation error message on username

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -20,6 +20,7 @@ export default class RegistrationForm{
         el.previousValue = el.value
     }
     usernameHandler(){
+        this.username.errors = false
         this.usernameImmediately()
         clearTimeout(this.username.timer)
         this.username.timer = setTimeout(()=> this.usernameAfterDelay(),3000)
@@ -28,10 +29,17 @@ export default class RegistrationForm{
         if (this.username.value != "" && !/^([a-zA-Z0-9]+)$/.test(this.username.value)){
             this.showValidationError(this.username,"Username can only contain letters and numbers")
         }
+        if (!this.username.errors){
+            this.hideValidationError(this.username)
+        }
+    }
+    hideValidationError(el){
+        el.nextElementSibling.classList.remove("liveValidateMessage--visible")
     }
     showValidationError(el, message){
         el.nextElementSibling.innerHTML = message
         el.nextElementSibling.classList.add("liveValidateMessage--visible")
+        el.errors = true
     }
     usernameAfterDelay(){
         
