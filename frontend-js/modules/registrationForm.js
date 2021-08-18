9a1c46d673e2ba6d8d766aa5cb52188bf503670e
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-18 21:17:01 +0300

    implementing registration form handlers

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -1,10 +1,31 @@
 export default class RegistrationForm{
     constructor(){
+        this.allFields =  document.querySelectorAll("#registration-form .form-control")
+        this.insertValidationElements()
+        this.username = document.querySelector("#username-register")
+        this.username.previousValue = ""
         this.events()
     }
     //Events
     events(){
-        alert("Registration JS is running")
+        this.username.addEventListener("keyup" ,() => {
+            this.isDifferent(this.username, this.usernameHandler)
+        })
     }
     //Methods
+    isDifferent(el, handler){
+        if (el.previousValue != el.value){
+            handler.call(this)
+        }
+        el.previousValue = el.value
+    }
+    usernameHandler(){
+        alert("Username handler just ran")
+    }
+
+    insertValidationElements(){
+        this.allFields.forEach(function(el){
+            el.insertAdjacentHTML('afterend','<div class = "alert alert-danger small liveValidateMessage" ></div>')
+        })
+    }
 }
