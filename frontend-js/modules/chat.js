ae91b4b12679e8d7bd595fb8e9e7d310d490091b
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-18 13:47:17 +0300

    linking chat to profile

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -62,9 +62,9 @@ export default class Chat {
     displayMessageFromServer(data){
         this.chatLog.insertAdjacentHTML('beforeend',DOMPurify.sanitize(`
         <div class="chat-other">
-        <a href="#"><img class="avatar-tiny" src="${data.avatar}"></a>
+        <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
         <div class="chat-message"><div class="chat-message-inner">
-          <a href="#"><strong>${data.username}:</strong></a>
+          <a href="/profile/${data.username}"><strong>${data.username}:</strong></a>
           ${data.message}
         </div></div>
       </div>
