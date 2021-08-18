226dbc2117c0a8f7c0eec2c0ac1584336d6dcdfd
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-18 20:37:16 +0300

    initializing live validation for registration

---
+++ frontend-js/modules/registrationForm.js
@@ -0,0 +1,10 @@
+export default class RegistrationForm{
+    constructor(){
+        this.events()
+    }
+    //Events
+    events(){
+        alert("Registration JS is running")
+    }
+    //Methods
+}
