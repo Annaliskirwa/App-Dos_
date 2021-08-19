d53bb49569e5c555aa462a65cb38cb8194496ec4
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-19 18:06:07 +0300

    json webtoken for login authentication

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const Post = require('../models/Post')
 const Follow = require('../models/Follow')
+const jwt = require('jsonwebtoken')
 
 exports.doesUsernameExist = function(req, res){
   User.findByUsername(req.body.username).then(function(){
@@ -64,9 +65,9 @@ exports.login = function(req, res) {
 exports.apiLogin = function(req, res) {
   let user = new User(req.body)
   user.login().then(function(result) {
-    res.json("That is the username and password")
+    res.json(jwt.sign({_id: user.data._id}, process.env.JWTSECRET, {expiresIn:'7d'}))
   }).catch(function(e) {
-    res.json("Your username and password are incorrect")
+    res.json()
   })
 }
 
