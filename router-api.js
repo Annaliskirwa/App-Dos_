9cbd53bc2dec4982192b2374368b259307c6f29f
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-19 20:09:19 +0300

    configuring CORS

--- router-api.js
+++ router-api.js
@@ -2,6 +2,9 @@ const apiRouter = require('express').Router()
 const userController = require('./Controllers/userController')
 const postController = require('./Controllers/postController')
 const followController = require('./Controllers/followController')
+const cors = require ('cors')
+
+apiRouter.use(cors())
 
 apiRouter.post('/login', userController.apiLogin)
 apiRouter.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreate)
