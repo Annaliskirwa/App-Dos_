eb3cd7428543e30a9ebd716233eff9bf791c4ba1
Annaliskirwa <annaliskirwa@gmail.com>
2021-08-19 20:02:13 +0300

    api route for getting posts by a specific author

--- router-api.js
+++ router-api.js
@@ -6,5 +6,6 @@ const followController = require('./Controllers/followController')
 apiRouter.post('/login', userController.apiLogin)
 apiRouter.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreate)
 apiRouter.delete('/post/:id', userController.apiMustBeLoggedIn, postController.apiDelete)
+apiRouter.get('/postsByAuthor/:username',userController.getPostsByUsername)
 
 module.exports = apiRouter
